697f3b607d710f21356eb0aa0330ae5a9106f98c
quirks91 <quirks91@gmail.com>
2023-11-14 16:39:31 +0900

    Feat: 블로그 타이틀 변경

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import Template from "@/components/layout/Template";
 import StyledComponentsRegistry from "@/styles/registry";
 
 export async function generateMetadata() {
-  const title = "Quirks91 | Dev | Tech | Archive";
+  const title = "Quirks | Archive";
   const description = "초보 블로거로 개발, 기술, 잡담 등 생각나는 여러 글을 작성합니다.";
 
   return {
